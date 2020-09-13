35177f1f0c2c045cb78fcaf66a481726e8715732
牛兴雷 <niuxinglei@niuxingleideMBP.lan>
2020-09-14 01:32:36 +0800

    完成定制文件

--- testos/installos/shell/install.sh
+++ testos/installos/shell/install.sh
@@ -4,7 +4,7 @@
 
 system=`grep "system" ../../test2.txt | awk -F: '{print $NF}'` 
 banben=`grep "banben" ../../test2.txt | awk -F: '{print $NF}'`
-banben=`grep "banben" ../../test2.txt | awk -F: '{print $NF}'`
-banben=`grep "banben" ../../test2.txt | awk -F: '{print $NF}'`
-banben=`grep "banben" ../../test2.txt | awk -F: '{print $NF}'`
+lang=`grep "lang" ../../test2.txt | awk -F: '{print $NF}'`
+fangan=`grep "fangan" ../../test2.txt | awk -F: '{print $NF}'`
+boot=`grep "boot" ../../test2.txt | awk -F: '{print $NF}'`
 echo $pinpai
