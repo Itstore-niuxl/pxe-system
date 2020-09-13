b988d31ae64c9f5e5dcedd7ccc3dc172548ae9b5
牛兴雷 <niuxinglei@niuxingleideMacBook-Pro.local>
2020-09-14 01:25:13 +0800

    脚本

---
+++ testos/installos/shell/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#author:niuxl
+#data:2020-09-10
+
+system=`grep "system" ../../test2.txt | awk -F: '{print $NF}'` 
+banben=`grep "banben" ../../test2.txt | awk -F: '{print $NF}'`
+banben=`grep "banben" ../../test2.txt | awk -F: '{print $NF}'`
+banben=`grep "banben" ../../test2.txt | awk -F: '{print $NF}'`
+banben=`grep "banben" ../../test2.txt | awk -F: '{print $NF}'`
+echo $pinpai
